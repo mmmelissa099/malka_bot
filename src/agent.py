7bc751b896802adb20b3ca3fe065620b6d905502
Melissa Braunstein <melissa.braunstein@gmail.com>
2026-09-17 16:10:49 -0300

    refactor(agent): change load_dotenv place

--- src/agent.py
+++ src/agent.py
@@ -1,14 +1,15 @@
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from langchain.agents import create_agent
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from tools import consultar_disponibilidad, buscar_en_faqs, escalar_a_humano
 from prompts import AGENT_SYSTEM_PROMPT
 
-load_dotenv()
+
 
 if not os.getenv("GOOGLE_API_KEY") or not os.getenv("GROQ_API_KEY"):
     raise RuntimeError("Falta GOOGLE_API_KEY o GROQ_API_KEY en tu .env")
